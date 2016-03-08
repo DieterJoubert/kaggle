e990cff0e2611955c4232e364ed9cfab8608fe39
DieterJoubert <dieterjoubert@gmail.com>
2016-03-07 22:11:23 -0800

    more R

--- titanic/r tutorial.R
+++ titanic/r tutorial.R
@@ -16,3 +16,39 @@ test$Survived <- rep(0, 418)
 #make a CSV file predicting that everybody dies
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "./out/theyallperish.csv", row.names = FALSE)
+
+#---------------------------------------------------------------------------
+
+#see how many women & men there are
+summary(train$Sex)
+
+#proportion table of gender vs. survival (divides each by total population)
+prop.table(table(train$Sex, train$Survived))
+
+#proportion table row-wise, the proportion of each sex that survived
+prop.table(table(train$Sex, train$Survived),1)
+
+#change test's survived category to be 1 if female, 0 if male
+test$Survived <- 0
+test$Survived [test$Sex == 'female'] <- 1
+
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "./out/allmenperish.csv", row.names = FALSE)
+
+#---------------------------------------------------------------------------
+
+#let's look at the age of passengers
+summary(train$Age)
+
+#create a new variable, age, to indicate whether the passenger is below age 18
+train$Child <- 0
+train$Child[train$Age < 18] <- 1
+
+#target Survived, see how sex and child variables affect it, how many Survived
+aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
+
+#same, but see how many total in each category double
+aggregate(Survived ~ Child + Sex, data=train, FUN=length)
+
+#same, but create custom function to get proportion for each double category
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
